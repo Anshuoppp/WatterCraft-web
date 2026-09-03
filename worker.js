87a4366fbef746b243ca335aebc9beda1dc56f40
Anshhu07 <97026054+Anshuoppp@users.noreply.github.com>
2026-09-04 00:21:03 +0530

    Fix function closing bracket in hmac

--- worker.js
+++ worker.js
@@ -141,5 +141,4 @@ async function hmac(secret, data) {
   const key = await crypto.subtle.importKey("raw", new TextEncoder().encode(secret), { name: "HMAC", hash: "SHA-256" }, false, ["sign"]);
   const sig = await crypto.subtle.sign("HMAC", key, new TextEncoder().encode(data));
   return [...new Uint8Array(sig)].map((b) => b.toString(16).padStart(2, "0")).join("");
-                                }
-
+}
